90bfb70f09fa7522a020f9d774dab14ef48fd148
Babloo Kumar <babloo.kumar.cse14@iitbhu.ac.in>
2026-02-17 16:03:46 +0530

    Add script to trigger GitHub workflow

---
+++ run-workflow.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+workflow_type="$1"
+case "$workflow_type" in
+java | llm | python | sh | sql)
+  INPUTS="{\"filename\":\"$2\"}"
+  ;;
+spark)
+  INPUTS="{\"language\":\"$2\",\"filename\":\"$3\"}"
+  ;;
+*)
+  echo "Invalid value for workflow type."
+  exit 1
+  ;;
+esac
+
+curl -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/run-${workflow_type}.yml/dispatches" \
+  -d "{\"ref\":\"master\",\"inputs\":$INPUTS}"
